397ef21e497d627ec033a019362feef1f6557668
sangeeth-Chejerla <sangeethreddychejerla@gmail.com>
2024-03-04 23:06:26 +0530

    Versioning

--- next.config.js
+++ next.config.js
@@ -34,12 +34,5 @@ const nextConfig = {
 
 module.exports = withMDX(nextConfig);
 
-module.exports = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-}
 
 
